fde9b090b1039ddc92c4aff4b83d39d78e025e4b
foxshley <andriana.hadisurya@gmail.com>
2025-04-25 15:14:07 +0700

    :recycle: refactor(backend): Decouple provider with auth service and change naming

--- backend/src/services/auth/index.ts
+++ backend/src/services/auth/index.ts
@@ -1,19 +1,5 @@
 import Elysia, { Context } from "elysia";
-import { auth } from "./providers/betterAuth";
+import { BetterAuthProvider } from "./providers/betterAuth";
 
 export const AuthService = new Elysia({ name: "Service.Auth" })
-  .mount(auth.handler)
-  .macro({
-    auth: {
-      async resolve({ error, request: { headers }}) {
-        const session = await auth.api.getSession({ headers });
-  
-        if(!session) return error(401);
-  
-        return {
-          user: session.user,
-          session: session.session
-        }
-      }
-    }
-  });
+  .use(BetterAuthProvider);
--- backend/src/services/auth/providers/betterAuth.ts
+++ backend/src/services/auth/providers/betterAuth.ts
@@ -1,6 +1,8 @@
+import Elysia from "elysia";
+
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "../../../db/providers/bun-sql";
+import { db } from "../../../db/providers/postgres";
 import { account, session, user, verification } from "../../../db/schema/auth";
 
 export const auth = betterAuth({
@@ -12,3 +14,20 @@ export const auth = betterAuth({
     enabled: true
   },
 })
+
+export const BetterAuthProvider = new Elysia({ name: "AuthProvider.BetterAuth" })
+  .mount(auth.handler)
+  .macro({
+    auth: {
+      async resolve({ error, request: { headers }}) {
+        const session = await auth.api.getSession({ headers });
+  
+        if(!session) return error(401);
+  
+        return {
+          user: session.user,
+          session: session.session
+        }
+      }
+    }
+  });
