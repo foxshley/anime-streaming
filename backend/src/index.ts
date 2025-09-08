b40c11ed05d40b6cc29b8e0c7f3489e7111ac989
foxshley <andriana.hadisurya@gmail.com>
2025-09-08 16:15:36 +0700

    :hammer_and_wrench: feat(backend): Integrate swagger for API docs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,36 +1,44 @@
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
 import { AuthService } from "./services/auth";
 import { logger } from "./utils/logger";
 
 const app = new Elysia()
-  .use(logger)
-  .onError(({ code, error, set }) => {
-    switch(code) {
-      case 'NOT_FOUND':
-        set.status = 404;
+	.use(logger)
+	.use(swagger())
+	.onError(({ code, error, set }) => {
+		switch (code) {
+			case "NOT_FOUND":
+				set.status = 404;
 
-        return {
-          status: 'error',
-          message: error.message
-        }
-      case 'INTERNAL_SERVER_ERROR':
-        set.status = 500;
+				return {
+					status: "error",
+					message: error.message,
+				};
+			case "INTERNAL_SERVER_ERROR":
+				set.status = 500;
 
-        return {
-          status: 'error',
-          message: error.message
-        }
+				return {
+					status: "error",
+					message: error.message,
+				};
 
-      case 'VALIDATION':
-        set.status = 422;
+			case "VALIDATION":
+				set.status = 422;
 
-        return {
-          status: 'error',
-          message: error.message
-        }
-    }
-  })
-  .use(AuthService)
-  .get("/", () => "Hello Elysia")
-  .listen(3000);
+				return {
+					status: "error",
+					message: error.message,
+				};
+		}
+	})
+	.use(AuthService)
+	.get("/", () => "Hello Elysia")
+	.get("/test", () => {
+		return {
+			status: "success",
+			message: "Hello Elysia",
+		};
+	})
+	.listen(process.env.PORT ?? 4000);
