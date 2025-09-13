b3b8fce241a6f8d6aadef90f7bf77da09599e3c6
foxshley <andriana.hadisurya@gmail.com>
2025-09-13 14:11:22 +0700

    :recycle: refactor(backend): Split app instance with server instance and refactor the error handling response in standarized format

---
+++ backend/src/app.ts
@@ -0,0 +1,62 @@
+import { Elysia } from "elysia";
+import { AnimeService } from "./services/anime";
+import { AuthService } from "./services/auth";
+
+export const app = new Elysia()
+	.onError(({ code, error, set }) => {
+		switch (code) {
+			case "NOT_FOUND":
+				set.status = 404;
+
+				return {
+					errors: [
+						{
+							status: set.status,
+							title: "Not Found",
+							detail: `The requested resource was not found.`,
+						},
+					],
+				};
+			case "VALIDATION":
+				set.status = 422;
+
+				return {
+					errors: [
+						{
+							status: set.status,
+							title: "Validation Error",
+							detail: `There were validation errors with the request.\n${error.detail(error.message)}`,
+						},
+					],
+				};
+
+			case "INTERNAL_SERVER_ERROR":
+				set.status = 500;
+
+				return {
+					errors: [
+						{
+							status: set.status,
+							title: "Internal Server Error",
+							detail: `An unexpected error occurred on the server.\n${error.message}`,
+						},
+					],
+				};
+
+			case "UNKNOWN":
+				set.status = 520;
+
+				return {
+					errors: [
+						{
+							status: set.status,
+							title: "Unknown Error",
+							detail: `An unknown error occurred on the server.\n${error.message}`,
+						},
+					],
+				};
+		}
+	})
+	.use(AuthService)
+	.use(AnimeService)
+	.get("/", () => "Hello Elysia")
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,39 +1,10 @@
 import { openapi } from "@elysiajs/openapi";
-import { Elysia } from "elysia";
-import { AnimeService } from "./services/anime";
-import { AuthService } from "./services/auth";
 import { logger } from "./utils/logger";
+import { app } from "./app";
+import { Elysia } from "elysia";
 
-const _app = new Elysia()
+const _server = new Elysia()
 	.use(logger)
 	.use(openapi())
-	.onError(({ code, error, set }) => {
-		switch (code) {
-			case "NOT_FOUND":
-				set.status = 404;
-
-				return {
-					status: "error",
-					message: error.message,
-				};
-			case "INTERNAL_SERVER_ERROR":
-				set.status = 500;
-
-				return {
-					status: "error",
-					message: error.message,
-				};
-
-			case "VALIDATION":
-				set.status = 422;
-
-				return {
-					status: "error",
-					message: error.message,
-				};
-		}
-	})
-	.use(AuthService)
-	.use(AnimeService)
-	.get("/", () => "Hello Elysia")
+	.use(app)
 	.listen(process.env.PORT ?? 4000);
