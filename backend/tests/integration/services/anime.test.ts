61787fc6337ce159f7028c92ac70d66c37e86c4c
foxshley <andriana.hadisurya@gmail.com>
2025-09-13 15:21:35 +0700

    :white_check_mark: test(backend): Add validation test for POST and PATCH routes

--- backend/tests/integration/services/anime.test.ts
+++ backend/tests/integration/services/anime.test.ts
@@ -1,9 +1,9 @@
 import { describe, expect, it } from "bun:test";
-import { AnimeService } from "../../../src/services/anime";
+import { app } from "../../../src/app";
 
 describe("Anime Service", () => {
 	it("GET /anime/", async () => {
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/"),
 		).then((res) => res.json());
 
@@ -15,7 +15,7 @@ describe("Anime Service", () => {
 	});
 
 	it("GET /anime/:id", async () => {
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/1"),
 		).then((res) => res.json());
 
@@ -26,7 +26,7 @@ describe("Anime Service", () => {
 
 	it("GET /anime/:id - Not Found", async () => {
 		// Mock the repository methods
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/9999"),
 		).then((res) => res.json());
 
@@ -49,7 +49,7 @@ describe("Anime Service", () => {
 			ageRating: "PG-13",
 		};
 
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/", {
 				method: "POST",
 				headers: { "Content-Type": "application/json" },
@@ -62,13 +62,40 @@ describe("Anime Service", () => {
 		expect(response.message).toBe("Anime created successfully");
 	});
 
+	it("POST /anime/ - Validation Error", async () => {
+		const newAnimeData = {
+			title: 2342432,
+			originalTitle: "デート・ア・ライブ",
+			description: "A story about dating spirits to save the world.",
+			coverImageUrl: "",
+			bannerImageUrl: "",
+			release_year: 2013,
+			status: "ongoing",
+			totalEpisodes: 12,
+			averageRating: "4.0",
+			ageRating: "PG-13",
+		};
+
+		const response = await app.handle(
+			new Request("http://localhost/anime/", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ data: newAnimeData }),
+			}),
+		).then((res) => res.json());
+
+		expect(response.errors).toBeInstanceOf(Array);
+		expect(response.errors[0].status).toBe(422);
+		expect(response.errors[0].title).toBe("Validation Error");
+	});
+
 	it("PATCH /anime/:id", async () => {
 		const updateData = {
 			title: "Date A Live II",
 			totalEpisodes: 24,
 		};
 
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/3", {
 				method: "PATCH",
 				headers: { "Content-Type": "application/json" },
@@ -87,7 +114,7 @@ describe("Anime Service", () => {
 			title: "Non-Existent Anime",
 		};
 
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/9999", {
 				method: "PATCH",
 				headers: { "Content-Type": "application/json" },
@@ -100,8 +127,26 @@ describe("Anime Service", () => {
 		expect(response.errors[0].title).toBe("Not Found");
 	});
 
+	it("PATCH /anime/:id - Validation Error", async () => {
+		const updateData = {
+			title: 2342432,
+		};
+
+		const response = await app.handle(
+			new Request("http://localhost/anime/3", {
+				method: "PATCH",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ data: updateData }),
+			}),
+		).then((res) => res.json());
+
+		expect(response.errors).toBeInstanceOf(Array);
+		expect(response.errors[0].status).toBe(422);
+		expect(response.errors[0].title).toBe("Validation Error");
+	});
+
 	it("DELETE /anime/:id", async () => {
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/3", {
 				method: "DELETE",
 			}),
@@ -111,7 +156,7 @@ describe("Anime Service", () => {
 	});
 
 	it("DELETE /anime/:id - Not Found", async () => {
-		const response = await AnimeService.handle(
+		const response = await app.handle(
 			new Request("http://localhost/anime/9999", {
 				method: "DELETE",
 			}),
