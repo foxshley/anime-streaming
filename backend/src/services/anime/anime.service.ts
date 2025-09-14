9fcc87566f968ad5bc4fbadeb870080f5f749234
foxshley <andriana.hadisurya@gmail.com>
2025-09-14 18:01:42 +0700

    :art: style(backend): Fix linting and formatting issue

--- backend/src/services/anime/anime.service.ts
+++ backend/src/services/anime/anime.service.ts
@@ -32,7 +32,7 @@ export abstract class Anime {
 	static async getAnimeById(id: number): Promise<AnimeType> {
 		const anime = await AnimeRepository.findById(id);
 		if (!anime) throw new NotFoundError("Anime not found");
-		
+
 		return anime;
 	}
 
